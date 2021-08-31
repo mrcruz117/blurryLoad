7a0c79ae2d77b621f230f94e764e6129f8cf390c
Michael Cruz <mrcruz1989@gmail.com>
2021-08-31 15:04:32 -0400

    js complete

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ function blurring() {
 
   if (load > 99) clearInterval(int);
 
-  loadText.innerText = `${load}%`
-  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-}
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
